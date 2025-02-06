9cacf794518dbc8fd056020ae62e211e9d61a1a2
top1ordie <top1ordie@gmail.com>
2025-02-07 03:40:37 +0500

    modiff nvim conf

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,3 @@
 require("config.lazy")
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
+require("config.keys")
+require("config.optins")
